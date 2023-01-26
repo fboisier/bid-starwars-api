31df0285e7b6221ddbee1a8f76f4533d2298eb9a
Francisco Boisier <fboisier@MacBook-Pro-de-Francisco.local>
2023-01-25 22:32:11 -0300

    star wars con axios

--- src/components/StarWars.jsx
+++ src/components/StarWars.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export const StarWars = () => {
   const [planetas, setPlanetas] = useState([]);
   const buscarDatos = () =>{
     
-    fetch("https://swapi.dev/api/planets/")
-    .then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.results);
-        setPlanetas(response.results.map((planeta)=> ({nombre: planeta.name, poblacion: planeta.population})));
-    }).catch(err => {
-        console.log(err);
-    });
+    // fetch("https://swapi.dev/api/planets/")
+    // .then(response => {
+    //     return response.json();
+    // }).then(response => {
+    //     console.log(response.results);
+    //     setPlanetas(response.results.map((planeta)=> ({nombre: planeta.name, poblacion: planeta.population})));
+    // }).catch(err => {
+    //     console.log(err);
+    // });
 
+    axios.get('https://swapi.dev/api/planets/').then(response=>{
+      setPlanetas(response.data.results.map((planeta)=> ({nombre: planeta.name, poblacion: planeta.population})));
+    })
   }
+
+
+  useEffect(() => {
+    buscarDatos();
+  }, [])
+  
+
+
   return (
     <>
         <button onClick={buscarDatos}>Obtener Planetas!</button>
